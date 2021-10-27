f4e917cd26bf8940324241d0f1a7010a092ef3b1
Guillem <guillem@disedit.com>
2021-10-27 13:54:25 +0200

    Update manrope

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,7 @@ export default defineNuxtConfig({
         { rel: 'icon', type: 'image/x-icon', href: 'https://compromis.net/sub/icons/favicon.ico' },
         { rel: 'icon', type: 'image/png', sizes: '32x32', href: 'https://compromis.net/sub/icons/favicon32.png' },
         { rel: 'icon', type: 'image/png', sizes: '192x192', href: 'https://compromis.net/sub/icons/favicon192.png' },
-        { rel: 'stylesheet', href: 'https://compromis.net/sub/fonts/compromis.css?variable2021' }
+        { rel: 'stylesheet', href: 'https://compromis.net/sub/fonts/manrope.css?variable' }
       ]
   }
 })
